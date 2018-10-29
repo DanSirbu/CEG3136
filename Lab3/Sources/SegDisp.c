99333d98aec41ad09f334c560ad67b5cdf5b3d3a
Dan Sirbu <dsirb066@gmail.com>
2018-10-29 13:52:15 -0400

    t

--- Lab3/Sources/SegDisp.c
+++ Lab3/Sources/SegDisp.c
@@ -69,8 +69,8 @@ void segDisp(void)
 	// Complete this function
 	int i;
 	for(i=0; i<4; i++) {
-    PTP = ~(1 << i);
-    PORTB = NUM_TO_7_SEG_TBL[CHAR_TO_NUM(dispChars[i])];
-    delayms(1);
+        PTP = 0;//~(1 << i);
+        PORTB = NUM_TO_7_SEG_TBL[CHAR_TO_NUM(dispChars[i])];
+        delayms(100);
   } 
 }
