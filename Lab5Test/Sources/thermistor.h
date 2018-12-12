ac16ef11807ac3e078337a5ddae7b381937be6e1
Dan Sirbu <dsirb066@gmail.com>
2018-12-11 20:25:22 -0500

    Removed unused project

--- Lab5Test/Sources/thermistor.h
+++
@@ -1,8 +0,0 @@
-/*----------------
-File: Thermistor.h
-Description: Header file for Thermistor Module
---------------------*/
-
-// Function Prototypes
-void initThermistor(void);
-int getTemp(void);
